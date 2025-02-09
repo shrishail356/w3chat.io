c8f6f665cf4390b05328baa1c8e4dcf4051fc3d4
Shrishail Patil <91820178+shrishail356@users.noreply.github.com>
2025-02-09 22:00:05 +0530

    Updated code

--- src/app/scheduler/page.tsx
+++ src/app/scheduler/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
-import NavBar from "@/components/Navbar";
+import NavBar from "../../components/Navbar";
 import Footer from "@/components/Landingpage/Footer";
 import {
   Mail,
